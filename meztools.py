ac140a7960bd22d7e2dcc3b53cb3fcfb5e91c0b9
Marco Gomez <29342489+mgomezAstro@users.noreply.github.com>
2022-06-22 16:26:58 -0700

    MEZTools converted into a class.

---
+++ meztools.py
@@ -0,0 +1,110 @@
+from typing import Optional, Tuple, Union
+import aplpy
+from astropy.io import fits
+from astropy.io.fits import HDUList
+import matplotlib.pyplot as plt
+
+
+# Light speed in km/s
+c_ = 299792.458
+
+
+class MEZTools:
+    def __init__(self, fitsfile: str) -> None:
+        self.fitsfile = fitsfile
+
+    def pixTovel(
+        self,
+        w0: float,
+        scale: float,
+        vsys: float = 0.0,
+        crsum: int = None,
+        ccsum: int = None,
+    ) -> HDUList:
+        """Convert the wavelenght and pixels into velocity and arcsec, respectively."""
+
+        hdu = fits.open(self.fitsfile)
+        header = hdu[0].header
+
+        if crsum is None or ccsum is None:
+            if "CCDSUM" in header:
+                binning = header["CCDSUM"]
+                ccsum = float(binning[0])
+                crsum = float(binning[2])
+            elif "CBIN" in header or "RBIN" in header:
+                ccsum = float(header["CBIN"])
+                crsum = float(header["RBIN"])
+            else:
+                raise ValueError(
+                    "You must specify 'crsum' or 'ccsum' if binning information is not in header."
+                )
+
+        ax1 = 2
+        ax2 = 1
+        if header["DISPAXIS"] == 2:
+            ax1 = 1
+            ax2 = 2
+
+        header[f"CRVAL{ax1}"] = -(header[f"NAXIS{ax1}"] / 2.0) * scale * crsum
+        header[f"CRPIX{ax1}"] = 0
+        header[f"CD{ax1}_{ax1}"] = scale * crsum
+        header[f"CDELT{ax1}"] = scale * crsum
+        dv = (header[f"CDELT{ax2}"] / w0) * c_
+        header[f"CDELT{ax2}"] = dv
+        header[f"CD{ax2}_{ax2}"] = dv
+        v0 = (header[f"CRVAL{ax2}"] - w0) / w0 * c_ + header["VLSR"] - vsys
+        header[f"CRVAL{ax2}"] = v0
+
+        header["VSYS"] = (vsys, "Systemic velocity.")
+
+        return hdu
+
+    def vcut(self):
+        pass
+
+    def save_fits(self, outname: str) -> None:
+        pass
+
+
+def plotOneSpec(
+    fitsfile: Union[str, HDUList],
+    dimensions: Tuple[int] = [1, 0],
+    vmin: Optional[float] = None,
+    vmax: Optional[float] = None,
+    show: bool = True,
+    save: bool = False,
+    xlabel: str = "X axis",
+    ylabel: str = "Y axis",
+    xspacing: Optional[float] = None,
+    yspacing: Optional[float] = None,
+    recenter: Optional[Tuple[float]] = None,
+) -> None:
+    """Funtion to plot a spectrum."""
+
+    if isinstance(fitsfile, str):
+        hdu = fits.open(fitsfile)
+    if isinstance(fitsfile, HDUList):
+        hdu = fitsfile
+        del fitsfile
+
+    fig = aplpy.FITSFigure(hdu, dimensions=dimensions)
+    fig.show_grayscale(invert=True, vmin=vmin, vmax=vmax)
+    fig.set_theme("publication")
+
+    fig.axis_labels.set_xtext(xlabel)
+    fig.axis_labels.set_ytext(ylabel)
+
+    fig.tick_labels.set_font(size=11)
+    if xspacing is not None:
+        fig.ticks.set_xspacing(xspacing)
+    if yspacing is not None:
+        fig.ticks.set_yspacing(yspacing)
+
+    if recenter is not None:
+        fig.recenter(recenter[0], recenter[1], width=recenter[2], height=recenter[3])
+
+    if save and not show:
+        fig.savefig(f"{fitsfile.split('.')[0]}_vel.fits", overwrite=True)
+
+    if show:
+        plt.show()
